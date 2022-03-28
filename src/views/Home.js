0b5ab8169cd7e41fa00d1d4040ca3b74cf5375f9
luisfeliperc1994@gmail.com <epz0-MHNae4tsV73yC2e$xKOf>
2022-03-28 15:45:12 -0300

    teste 1

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
         return (
             <View style={styles.home}>
                 <View style={styles.container_botao}>
-                    <Button title="Criar orçamento teste" onPress={() => this.props.navigation.navigate('Criar')}/>
+                    <Button title="Criar orçamento teste 1" onPress={() => this.props.navigation.navigate('Criar')}/>
                 </View>
                 <View style={styles.container_botao}>
                     <Button title="Ver lista de orçamentos" onPress={() => this.props.navigation.navigate('Lista')}/>
